97c394d1293b305ad0dcd4078121436202bd25d5
Andy <Andy@DESKTOP-7SDVNDO>
2021-11-14 14:32:43 -0500

    update main proc for full functional

--- AnthFileCoder/src/com/barabbas1231/AnthFileCoder/AnthFileCoder.java
+++ AnthFileCoder/src/com/barabbas1231/AnthFileCoder/AnthFileCoder.java
@@ -19,9 +19,15 @@ public class AnthFileCoder {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 			char fileChar;
+			StringBuilder strwr = new StringBuilder();
 			FileLogisticsWorker flw = new FileLogisticsWorker(args[0]);
+			DataWriter dw = new DataWriter(args[0]);
 			while (flw.getAvailable() != -1) {
 				fileChar = flw.getChar();
+				
+				strwr.append(fileChar);
+				
+				dw.write(strwr.toString());
 			}
 
 
